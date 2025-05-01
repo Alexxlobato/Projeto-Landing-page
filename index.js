bc7d27e2309af24f5f6dd5384d80554b6a67d346
Alex Lobato <alex@copinhodobem.com.br>
2025-05-01 09:07:58 -0300

    Versão inicial

---
+++ index.js
@@ -0,0 +1,22 @@
+var SetaDireita = window.document.getElementById("seta-direita");
+var Leonardo = window.document.getElementById("Leonardo");
+var Samantha = window.document.getElementById("Samantha");
+var Bruna = window.document.getElementById("Bruna");
+var SetaEsquerda = window.document.getElementById("seta-esquerda");
+
+function RolarParaDireita() {
+    Leonardo.style.display="none";
+    Bruna.style.display="flex";
+    SetaDireita.style.display="none";
+    SetaEsquerda.style.display = "flex";
+    SetaEsquerda.style.marginTop = "85px";
+
+}
+function RolarParaEsquerda() {
+    Leonardo.style.display="flex";
+    Bruna.style.display="none";
+    SetaDireita.style.display="flex";
+    SetaDireita.style.marginTop = "85px";
+    SetaEsquerda.style.display="none";
+}
+
